7831f973bf141f9a209575d7d124069bc69d9281
Kartheek <sunkara.durga.varaha.satya.jagadeeswara@ibm.com>
2019-08-13 17:38:44 +0530

    Initial Commit

---
+++ jenktestproj/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is a test sample for jenkins");
+            Console.ReadKey();
+        }
+    }
+}
